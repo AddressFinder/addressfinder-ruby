38d2a9537fbeebe331024ce5d82cb11885df8e4d
Alexandre Barret <alex@abletech.nz>
2015-11-24 13:26:06 +1300

    Update address_info and address_info_spec

--- spec/lib/addressfinder/address_info_spec.rb
+++ spec/lib/addressfinder/address_info_spec.rb
@@ -22,9 +22,15 @@ RSpec.describe AddressFinder::AddressInfo do
     end
 
     context 'with a country override' do
-      let(:args){ {pxid: '123'} }
+      let(:args){ {pxid: '123', country: 'nz'} }
 
-      it { expect(request_uri).to eq('/api/au/address/info.json?pxid=123&key=XXX&secret=YYY') }
+      it { expect(request_uri).to eq('/api/nz/address/info.json?pxid=123&key=XXX&secret=YYY') }
+    end
+
+    context 'with a key and secret override' do
+      let(:args){ {pxid: '123', key:'AAA', secret: 'BBB'} }
+
+      it { expect(request_uri).to eq('/api/au/address/info.json?pxid=123&key=AAA&secret=BBB') }
     end
   end
 
