241315215051d78017fee352ca4202e62b08fffb
Cassandre Guinut <cassandre.guinut@abletech.co.nz>
2021-09-24 14:40:25 +1200

    tweak Rspec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,9 @@ require 'addressfinder'
 
 RSpec.configure do |config|
   config.mock_with :rspec
+
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
 end
 
 WebMock.disable_net_connect!
